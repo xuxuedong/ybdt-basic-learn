e297a36f8336bea18478650615e7353a82c62e92
xuxuedong <yibudengtian@Outlook.com>
2020-01-03 16:52:29 +0800

    Update file_content_cut_repeat.py

--- 2019_12_09_渗透测试弱口令/file_content_cut_repeat.py
+++ 2019_12_09_渗透测试弱口令/file_content_cut_repeat.py
@@ -1,10 +1,11 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import sys
 
 def usage():
     if len(sys.argv) != 2:
-        print("Usage: python3 ./file_cut_repeat.py file_to_cut");
+        print("Usage: python3 ./file_content_cut_repeat.py file_to_cut");
         sys.exit("Goodbye!!!");
     else:
         global file_name;
@@ -13,8 +14,8 @@ def usage():
 def file_cut_repeat():
     f = open(file_name, "r");
     lines = f.readlines();
-    print(lines);
-    tmp = [];
+    # print(lines);
+    tmp = []; # 新建一个空数组，用于存储去重后的元素
     for line in lines:
         if line not in tmp:
             tmp.append(line);
@@ -28,4 +29,5 @@ def main():
     usage();
     file_cut_repeat();
 
-main();
+if __name__ == "__main__":
+    main();
